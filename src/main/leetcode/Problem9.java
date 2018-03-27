750fb445b3eded0470b6793674367cb3942d2991
Kevin Tan <kevin.tan97@hotmail.com>
2018-03-27 17:07:06 -0400

    problem 9

---
+++ src/main/leetcode/Problem9.java
@@ -0,0 +1,16 @@
+package main.leetcode;
+
+public class Problem9 {
+
+    private int testVal;
+
+    public Problem9(int testVal){
+        this.testVal = testVal;
+        System.out.println(solution(testVal));
+    }
+
+    public boolean solution(int x){
+        return false;
+    }
+
+}
