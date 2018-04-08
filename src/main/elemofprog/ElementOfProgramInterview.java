002dd3cf54098d7c92841637052bcae438fd70d0
Kevin Tan <kevin.tan97@hotmail.com>
2018-04-08 19:45:11 -0400

    element of prog

---
+++ src/main/elemofprog/ElementOfProgramInterview.java
@@ -0,0 +1,11 @@
+package main.elemofprog;
+
+public class ElementOfProgramInterview {
+
+    public ElementOfProgramInterview(){ }
+
+    public void run(){
+
+    }
+
+}
--- src/main/leetcode/Problem9.java
+++ src/main/leetcode/Problem9.java
@@ -4,14 +4,25 @@ public class Problem9 {
 
     private int testVal;
 
-    public Problem9(int testVal){
+    public Problem9(int testVal) {
         this.testVal = testVal;
-        System.out.println(solution(testVal));
+        System.out.println("Leetcode problem 9: input: " + testVal + " output: " + sol(testVal));
     }
 
     //Determine whether an integer is a palindrome. Do this without extra space.
-    boolean solution(int x){
+    private boolean sol(int x) {
+        int reverse = 0;
+        if(x<0)
+            return false;
+        while(x > 0){
+            if(reverse + x == 0)
+                return true;
+            reverse = reverse*10 - (x%10);
+            x /= 10;
+        }
         return false;
     }
 
 }
+
+//-21474 21474
